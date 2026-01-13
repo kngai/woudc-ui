1ac1bde8b80c0e98ba8a723a510571d8ffbfa69b
Kevin Ngai <kevin.ngai@canada.ca>
2026-01-13 13:15:06 -0800

    WIP data_access.vue update

--- pages/data/data_access.vue
+++ pages/data/data_access.vue
@@ -1,194 +1,259 @@
 <template>
   <v-container>
     <h1>{{ $t('data.access.title') }}</h1>
-    <v-row>
-      <v-col sm="12" md="8" cols="12">
-        <p>{{ $t('data.access.blurb.body-intro') }}</p>
-        <i18n path="data.access.blurb.body-accessibility" tag="p">
-          <template #contact-us>
-            <nuxt-link :to="localePath('contact')">
-              <span>{{ $t('common.contact-us') }}</span>
-            </nuxt-link>
-          </template>
-        </i18n>
-        <v-alert class="mb-4" color="info" outlined text border="left">
-          <h3>
-            {{ $t('data.access.note1.title') }}
-          </h3>
-          <i18n path="data.access.note1.body" tag="div">
-            <template #policy>
-              <nuxt-link :to="localePath('data-data_policy')">
-                <span>{{ $t('common.policy') }}</span>
-              </nuxt-link>
-            </template>
-          </i18n>
-        </v-alert>
-        <v-alert class="mb-4" color="info" outlined text border="left">
-          <h3>
-            {{ $t('data.access.note2.title') }}
-          </h3>
-          <i18n path="data.access.note2.body" tag="div">
-            <template #stations-page>
-              <nuxt-link :to="localePath('data-stations')">
-                <span>{{ $t('common.stations-page') }}</span>
-              </nuxt-link>
-            </template>
-          </i18n>
-        </v-alert>
-      </v-col>
-      <v-col sm="12" md="4" cols="12">
-        <v-card id="table-of-contents">
-          <v-card-title class="primary white--text">
-            {{ $t('data.access.contents.title') }}
-          </v-card-title>
-          <v-list id="contents-body" class="pa-0">
-            <v-list-item>
-              <v-list-item-title>
-                <nuxt-link to="#data-search-section">
-                  <span>{{ $t('data.access.contents.links.search') }}</span>
-                </nuxt-link>
-              </v-list-item-title>
-            </v-list-item>
-            <v-divider />
-            <v-list-item>
-              <v-list-item-title>
-                <nuxt-link to="#waf-section">
-                  <span>{{ $t('data.access.contents.links.waf') }}</span>
-                </nuxt-link>
-              </v-list-item-title>
-            </v-list-item>
-            <v-divider />
-            <v-list-item>
-              <v-list-item-title>
-                <nuxt-link to="#web-services-section">
-                  <span>{{ $t('data.access.ogcapi.title') }}</span>
-                </nuxt-link>
-              </v-list-item-title>
-            </v-list-item>
-            <v-divider />
-            <v-list-item>
-              <v-list-item-title>
-                <nuxt-link to="#examples-section">
-                  <span>{{ $t('data.access.contents.links.examples') }}</span>
-                </nuxt-link>
-              </v-list-item-title>
-            </v-list-item>
-            <v-divider />
-          </v-list>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <div id="data-search-section">
-          <h2>{{ $t('data.access.search.title') }}</h2>
-          <i18n path="data.access.search.blurb.body-intro" tag="p">
-            <template #search>
-              <nuxt-link :to="localePath('data-search')">
-                <span>{{ $t('common.search') }}</span>
-              </nuxt-link>
-            </template>
-          </i18n>
-          <i18n path="data.access.search.blurb.body-howto" tag="p">
-            <template #how-to>
-              <a :href="searchHelpURL" target="_blank">
-                {{ $t('data.access.search.how-to') }}
-                <v-icon x-small>mdi-open-in-new</v-icon>
-              </a>
-            </template>
-          </i18n>
-          <v-alert class="mb-4" color="info" outlined text border="left">
-            <div>
-              {{ $t('data.access.search.note') }}
-            </div>
-          </v-alert>
-        </div>
-        <div id="waf-section">
-          <h2>{{ $t('data.access.waf.title') }}</h2>
-          <i18n path="data.access.waf.blurb.body-intro" tag="p">
-            <template #waf>
-              <a :href="$config.WOUDC_UI_WAF_URL" target="_blank">
-                {{ $t('common.wafFull') }}
-                <v-icon x-small>mdi-open-in-new</v-icon>
-              </a>
-            </template>
-          </i18n>
-          <i18n path="data.access.waf.blurb.body-files" tag="p">
-            <template #summary>
-              <a :href="wafSummaryURL" target="_blank">
-                {{ $t('data.access.waf.summary') }}
-                <v-icon x-small>mdi-open-in-new</v-icon>
-              </a>
-            </template>
-          </i18n>
-          <i18n path="data.access.waf.blurb.body-howto" tag="p">
-            <template #how-to>
-              <a :href="wafGuideURL" target="_blank">
-                {{ $t('data.access.waf.how-to') }}
-                <v-icon x-small>mdi-open-in-new</v-icon>
-              </a>
-            </template>
-          </i18n>
-        </div>
-        <div id="web-services-section">
-          <h2>{{ $t('data.access.ogcapi.title') }}</h2>
-          <p>{{ $t('data.access.ogcapi.intro-api') }}</p>
-          <i18n path="data.access.ogcapi.intro-ogc" tag="p">
-            <template #ogc>
-              <a :href="ogcURL" target="_blank">
-                {{ $t('common.ogcThe') }}
-                <v-icon x-small>mdi-open-in-new</v-icon>
-              </a>
-            </template>
-          </i18n>
-          <v-alert class="mb-4" color="info" outlined text border="left">
-            <i18n path="data.access.ogcapi.intro-endpoint" tag="div">
-              <template #url>
-                <strong>
-                  <a :href="$config.WOUDC_UI_API_URL" target="_blank">
-                    {{ $config.WOUDC_UI_API_URL }}
-                    <v-icon x-small>mdi-open-in-new</v-icon>
-                  </a>
-                </strong>
-              </template>
-            </i18n>
-          </v-alert>
-          <p>{{ $t('data.access.ogcapi.intro-provides') }}</p>
-        </div>
 
-        <div id="examples-section">
-          <h2>{{ $t('data.access.examples.title') }}</h2>
-          <i18n path="data.access.examples.blurb" tag="p">
-            <template #github>
-              <a :href="githubURL" target="_blank">
-                {{ $t('common.github') }}
-                <v-icon x-small>mdi-open-in-new</v-icon>
-              </a>
-            </template>
-          </i18n>
+    <h2>{{ $t('data.access.apis') }}</h2>
 
-          <v-list id="example-list" dense>
-            <i18n path="data.access.examples.links.pywoudc" tag="v-list-item">
-              <template #pywoudc>
-                <a :href="examples.pywoudc" target="_blank">
-                  pywoudc
-                  <v-icon x-small>mdi-open-in-new</v-icon>
+    <p>{{ $t('data.access.webAPIs') }}</p>
+
+    <i18n path="data.access.ogcAPIs" tag="p">
+      <template #ogcStandards>
+        <a href="https://ogcapi.ogc.org" target="_blank">
+          {{ $t('about.standards.os_ogc') }}
+          <v-icon x-small>mdi-open-in-new</v-icon>
+        </a>
+      </template>
+    </i18n>
+
+    <i18n path="data.access.woudcAPI" tag="p">
+      <template #woudcAPILink>
+        <a :href="woudcAPIURL" target="_blank">
+          <span>{{ woudcAPIURL }}</span>
+          <v-icon x-small>mdi-open-in-new</v-icon>
+        </a>
+      </template>
+    </i18n>
+
+    <p>
+      {{ $t('data.access.apiLinksExamples') }}
+      <v-card class="mx-auto">
+        <v-list dense>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>
+                <a :href="exampleAPILinks.collections" target="_blank">
+                  {{ $t('data.access.viewCollections') }}
                 </a>
-              </template>
-            </i18n>
-
-            <i18n path="data.access.examples.links.notebooks" tag="v-list-item">
-              <template #notebooks>
-                <a :href="examples.notebooks" target="_blank">
-                  notebooks
-                  <v-icon x-small>mdi-open-in-new</v-icon>
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                {{ exampleAPILinks.collections }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>
+                <a :href="exampleAPILinks.discoveryMetadata" target="_blank">
+                  {{ $t('data.access.viewDiscoveryMetadata') }}
                 </a>
-              </template>
-            </i18n>
-          </v-list>
-        </div>
-      </v-col>
-    </v-row>
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                {{ exampleAPILinks.discoveryMetadata }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>
+                <a :href="exampleAPILinks.stations" target="_blank">
+                  {{ $t('data.access.viewStations') }}
+                </a>
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                {{ exampleAPILinks.stations }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>
+                <a :href="exampleAPILinks.stationBboxQuery" target="_blank">
+                  {{ $t('data.access.viewStationBbox') }}
+                </a>
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                {{ exampleAPILinks.stationBboxQuery }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>
+                <a :href="exampleAPILinks.singleStation" target="_blank">
+                  {{ $t('data.access.viewSingleStation') }}
+                </a>
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                {{ exampleAPILinks.singleStation }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>
+                <a :href="exampleAPILinks.contributors" target="_blank">
+                  {{ $t('data.access.viewContributors') }}
+                </a>
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                {{ exampleAPILinks.contributors }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>
+                <a :href="exampleAPILinks.countryContributors" target="_blank">
+                  {{ $t('data.access.viewCountryContributors') }}
+                </a>
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                {{ exampleAPILinks.countryContributors }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>
+                <a :href="exampleAPILinks.singleContributor" target="_blank">
+                  {{ $t('data.access.viewSingleContributor') }}
+                </a>
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                {{ exampleAPILinks.singleContributor }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>
+                <a :href="exampleAPILinks.dataRecords" target="_blank">
+                  {{ $t('data.access.viewDataRecords') }}
+                </a>
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                {{ exampleAPILinks.dataRecords }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>
+                <a
+                  :href="exampleAPILinks.totalOzoneBrewerDataRecords"
+                  target="_blank"
+                >
+                  {{ $t('data.access.viewTOBrewerDataRecords') }}
+                </a>
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                {{ exampleAPILinks.totalOzoneBrewerDataRecords }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </p>
+
+    <i18n path="data.access.swaggerUI" tag="p">
+      <template #swaggerURL>
+        <a :href="swaggerURL" target="_blank">{{ swaggerURL }}</a>
+        <v-icon x-small>mdi-open-in-new</v-icon>
+      </template>
+    </i18n>
+
+    <i18n path="data.access.poweredByPygeoapi" tag="p">
+      <template #pygeoapi>
+        <a href="https://pygeoapi.io" target="_blank">pygeoapi</a>
+        <v-icon x-small>mdi-open-in-new</v-icon>
+      </template>
+    </i18n>
+
+    <h2>{{ $t('data.access.toolsAndExamples') }}</h2>
+
+    <p>
+      {{ $t('data.access.exampleToolsForWOUDC') }}
+      <v-card class="mx-auto">
+        <v-list dense>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>
+                <a href="https://github.com/woudc/pywoudc" target="_blank">
+                  {{ $t('data.access.pywoudc') }}
+                </a>
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                {{ $t('data.access.pywoudcDesc') }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>
+                <a
+                  href="https://github.com/woudc/woudc/tree/master/notebooks"
+                  target="_blank"
+                >
+                  {{ $t('data.access.notebooks') }}
+                </a>
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                {{ $t('data.access.notebooksDesc') }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </p>
+
+    <h2>{{ $t('data.access.wmoInfoSystem') }}</h2>
+
+    <p>{{ $t('data.access.wis2Desc') }}</p>
+
+    <p>{{ $t('data.access.wis2Since') }}</p>
+
+    <p>{{ $t('data.access.wis2Guides') }}</p>
+
+    <ul>
+      <li>
+        <a href="https://library.wmo.int/idurl/4/68731" target="_blank">
+          {{ $t('data.access.wis2GuideManual') }}
+        </a>
+        <v-icon x-small>mdi-open-in-new</v-icon>
+      </li>
+      <li>
+        <a href="https://library.wmo.int/idurl/4/69130" target="_blank">
+          {{ $t('data.access.wis2Guide') }}
+        </a>
+        <v-icon x-small>mdi-open-in-new</v-icon>
+      </li>
+      <li>
+        <a href="https://library.wmo.int/idurl/4/69050" target="_blank">
+          {{ $t('data.access.wis2Transition') }}
+        </a>
+        <v-icon x-small>mdi-open-in-new</v-icon>
+      </li>
+      <li>
+        <a href="http://library.wmo.int/idviewer/57850/15" target="_blank">
+          {{ $t('data.access.wmoUnifiedDataPolicy') }}
+        </a>
+        <v-icon x-small>mdi-open-in-new</v-icon>
+      </li>
+    </ul>
+
+    <i18n path="data.access.wis2Cookbook" tag="p">
+      <template #wis2Cookbook>
+        <a
+          href="https://wmo-im.github.io/wis2-cookbook/cookbook/wis2-cookbook-DRAFT.html"
+          target="_blank"
+        ></a>
+        <v-icon x-small>mdi-open-in-new</v-icon>
+      </template>
+    </i18n>
+
+    <p>{{ $t('data.access.wis2WoudcDataShare') }}</p>
+
+    <h3>{{ $t('data.access.wis2DiscoveryAccess') }}</h3>
   </v-container>
 </template>
 
@@ -196,31 +261,19 @@
 export default {
   data() {
     return {
-      githubURL: 'https://github.com/woudc',
-      interoperabilityURL:
-        'https://www.wmo.int/pages/prog/www/WIS/documents/MOAWMO_OGC.pdf',
-      isoURL: 'https://www.isotc211.org/',
-      webServicesHowToURL:
-        'https://github.com/woudc/woudc/wiki/WebServicesHowto',
-      ogcStandardsURL: 'https://opengeospatial.org/standards/cat',
-      ogcURL: 'https://opengeospatial.org/',
-      searchHelpURL:
-        'https://github.com/woudc/woudc/wiki/DataSearchDownloadHowto',
-      wafGuideURL: 'https://github.com/woudc/woudc/wiki/WAFHowto',
-      wafSummaryURL: `${this.$config.WOUDC_UI_WAF_URL}/Summaries/dataset-snapshots`,
-      wfsURL: 'https://www.opengeospatial.org/standards/wfs',
-      wisURL: 'https://www.wmo.int/pages/prog/www/WIS/',
-      wmsURL: 'https://www.opengeospatial.org/standards/wms',
-      wpsURL: 'https://www.opengeospatial.org/standards/wps',
-      contentsSelectors: {
-        examples: 'examples-section',
-        search: 'data-search-section',
-        waf: 'waf-section',
-        web: 'web-services-section',
-      },
-      examples: {
-        pywoudc: 'https://github.com/woudc/pywoudc',
-        notebooks: 'https://github.com/woudc/woudc/tree/master/notebooks',
+      woudcAPIURL: this.$config.WOUDC_UI_API_URL,
+      swaggerURL: `${this.$config.WOUDC_UI_API_URL}/openapi`,
+      exampleAPILinks: {
+        collections: `${this.$config.WOUDC_UI_API_URL}/collections`,
+        discoveryMetadata: `${this.$config.WOUDC_UI_API_URL}/collections/discovery_metadata/items?lang=${this.$i18n.locale}`,
+        stations: `${this.$config.WOUDC_UI_API_URL}/collections/stations/items`,
+        stationBboxQuery: `${this.$config.WOUDC_UI_API_URL}/collections/stations/items?bbox=-142,42,-53,84`,
+        singleStation: `${this.$config.WOUDC_UI_API_URL}/collections/stations/LEO`,
+        contributors: `${this.$config.WOUDC_UI_API_URL}/collections/contributors/items`,
+        countryContributors: `${this.$config.WOUDC_UI_API_URL}/collections/contributors/items?country_name_en=Norway`,
+        singleContributor: `${this.$config.WOUDC_UI_API_URL}/collections/contributors/items/SMNA:WOUDC`,
+        dataRecords: `${this.$config.WOUDC_UI_API_URL}/collections/data-records/items`,
+        totalOzoneBrewerDataRecords: `${this.$config.WOUDC_UI_API_URL}/collections/data_records/items?instrument_name=brewer&content_category=TotalOzone`,
       },
     }
   },
@@ -246,15 +299,6 @@ export default {
     }
   },
   computed: {},
-  methods: {
-    prepareContentsLink(key) {
-      return {
-        text: this.$t('data.access.contents.links.' + key),
-        selector: this.contentsSelectors[key],
-        subsections: null,
-      }
-    },
-  },
   nuxtI18n: {
     paths: {
       en: '/data/data-access',
